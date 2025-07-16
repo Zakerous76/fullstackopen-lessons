f27a86b81cc0cb9aca131436ef7f392e67c23236
zakerous <m.z.humayoon@gmail.com>
2025-07-16 23:36:40 +0300

    Part 2 A started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Part1_C from "./Part1_C";
 import Part1_D from "./Part1_D";
+import Part2_A from "./Part2_A";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <title>FullStackOpen Lessons</title>
       <h1>Hello World</h1>
       {/* <Part1_C name="Zaker" age="29"></Part1_C> */}
-      <Part1_D />
+      {/* <Part1_D /> */}
+      <Part2_A />
     </>
   );
 }
---
+++ src/Part2_A.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Part2_A = () => {
+  return <div>Part2_A</div>;
+};
+
+export default Part2_A;
