18a7618bac9762fe85bbe72e414784fdee91d560
zakerous <m.z.humayoon@gmail.com>
2025-07-16 23:34:57 +0300

    Part 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Part1_C from "./Part1_C";
+import Part1_D from "./Part1_D";
 
 function App() {
   return (
     <>
       <title>FullStackOpen Lessons</title>
       <h1>Hello World</h1>
-      <Part1_C name="Zaker" age="29"></Part1_C>
+      {/* <Part1_C name="Zaker" age="29"></Part1_C> */}
+      <Part1_D />
     </>
   );
 }
