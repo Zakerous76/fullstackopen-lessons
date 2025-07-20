4f25ae6102ee942c685fa799912e77bd7f11d8db
zakerous <m.z.humayoon@gmail.com>
2025-07-20 10:10:43 +0300

    added a route for fetching a single note

--- backend-code/index.js
+++ backend-code/index.js
@@ -22,14 +22,28 @@ let notes = [
 
 const app = express();
 
+// Root
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+// All notes
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
+// A single note
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end("Requested note not found");
+  }
+});
+
+// Server Live
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
