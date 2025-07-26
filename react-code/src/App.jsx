c0779a55f1ac9aed60834a0654ae076e8aa6deca
zakerous <m.z.humayoon@gmail.com>
2025-07-26 11:10:42 +0300

    Start: Part 5B

--- react-code/src/App.jsx
+++ react-code/src/App.jsx
@@ -1,12 +1,13 @@
-import Part1_C from "./components/Part1_C";
-import Part1_D from "./components/Part1_D";
-import Part2_A from "./components/Part2_A";
-import Part2_B from "./components/Part2_B";
-import Part2_C from "./components/Part2_C";
+// import Part1_C from "./components/Part1_C";
+// import Part1_D from "./components/Part1_D";
+// import Part2_A from "./components/Part2_A";
+// import Part2_B from "./components/Part2_B";
+// import Part2_C from "./components/Part2_C";
 // import Part2_D from "./components/Part2_D";
-import Part2_E from "./components/Part2_E";
-import Part3_B from "./components/Part3_B";
-import Part5_A from "./components/Part5_A";
+// import Part2_E from "./components/Part2_E";
+// import Part3_B from "./components/Part3_B";
+// import Part5_A from "./components/Part5_A";
+import Part5_B from "./components/Part5_B";
 
 function App() {
   return (
@@ -20,7 +21,8 @@ function App() {
       {/* <Part2_D /> */}
       {/* <Part2_E /> */}
       {/* <Part3_B /> */}
-      <Part5_A />
+      {/* <Part5_A /> */}
+      <Part5_B />
     </>
   );
 }
---
+++ react-code/src/components/Part5_B.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import Note from "./Note";
+import noteService from "../services/notes";
+import Notification from "./Notification";
+import Footer from "./Footer";
+import LoginForm from "./LoginForm";
+import NotesForm from "./NotesForm";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [showAllToggle, setshowAllToggle] = useState(false);
+  const [showAllToggleText, setshowAllToggleText] = useState("Show All");
+  const [user, setUser] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+    const localUser = JSON.parse(
+      window.localStorage.getItem("loggedNoteAppUser")
+    );
+    if (localUser) {
+      setUser(localUser);
+    }
+  }, []);
+
+  const handleMakeImportant = (noteId) => () => {
+    const targetNote = notes.find((note) => note.id === noteId);
+    const updatedNote = { ...targetNote, important: !targetNote.important };
+
+    noteService
+      .update(updatedNote.id, updatedNote)
+      .then((returnedNote) => {
+        setNotes(
+          notes.map((note) => (note.id === noteId ? returnedNote : note))
+        );
+      })
+      .catch((err) => {
+        setErrorMsg(
+          `Fail: The note "${updatedNote.content}" is not found in the database. \n${err}`
+        );
+        setTimeout(() => {
+          setErrorMsg(null);
+        }, 5000);
+        setNotes(
+          notes.filter((note) => {
+            return note.id !== noteId;
+          })
+        );
+      });
+  };
+
+  const handleShowAllToggle = () => {
+    const newState = !showAllToggle;
+    setshowAllToggle(newState);
+    if (newState) {
+      setshowAllToggleText("Show Important");
+    } else {
+      setshowAllToggleText("Show All");
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    window.localStorage.removeItem("loggedNoteAppUser");
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMsg} />
+
+      {user === null ? (
+        <LoginForm setErrorMsg={setErrorMsg} setUser={setUser} />
+      ) : (
+        <div>
+          <h2>Welcome!</h2>
+          <p>
+            <b>{user.name}</b> logged-in
+          </p>
+          <p>
+            <button onClick={handleLogout}>Logout</button>
+          </p>
+          {<NotesForm setNotes={setNotes} />}
+        </div>
+      )}
+      <div className="notes-section">
+        <ul>
+          {notes.map((note) => {
+            return (
+              <Note
+                key={note.id}
+                note={note}
+                handleImportanceToggle={handleMakeImportant}
+              />
+            );
+          })}
+        </ul>
+      </div>
+      <button onClick={handleShowAllToggle}>{showAllToggleText}</button>
+      <Footer />
+    </div>
+  );
+};
+
+const Part5_B = () => {
+  return <App />;
+};
+
+export default Part5_B;
