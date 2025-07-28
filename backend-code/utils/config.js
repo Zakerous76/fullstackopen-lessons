197a70f89ae1ad9584c9236cf369fdf0e146d519
zakerous <m.z.humayoon@gmail.com>
2025-07-28 19:18:46 +0300

    Start: Part 5D

--- backend-code/utils/config.js
+++ backend-code/utils/config.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  console.log("TEST ENV DETECTED");
+  console.log("PORT:", process.env.PORT);
+  console.log("TEST_MONGODB_URI:", process.env.TEST_MONGODB_URI);
+}
 
 module.exports = { MONGODB_URI, PORT };
--- backend-code/utils/logger.js
+++ backend-code/utils/logger.js
@@ -1,13 +1,13 @@
 const info = (...param) => {
-  if (process.env.NODE_ENV !== "test") {
-    console.log(...param);
-  }
+  // if (process.env.NODE_ENV !== "test") {
+  console.log(...param);
+  // }
 };
 
 const error = (...param) => {
-  if (process.env.NODE_ENV !== "test") {
-    console.error(...param);
-  }
+  // if (process.env.NODE_ENV !== "test") {
+  console.error(...param);
+  // }
 };
 
 module.exports = { info, error };
