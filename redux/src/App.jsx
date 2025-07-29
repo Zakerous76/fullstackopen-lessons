5744c6c61433b908cee27a229d5cec1b9fdbb6f6
zakerous <m.z.humayoon@gmail.com>
2025-07-29 16:52:55 +0300

    Start: Part 6A

---
+++ redux/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { createStore } from "redux";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      case "ZERO":
+        return 0;
+      default: // if none of the above matches, code comes here
+        return state;
+    }
+  };
+
+  const store = createStore(counterReducer);
+
+  return <></>;
+}
+
+export default App;
