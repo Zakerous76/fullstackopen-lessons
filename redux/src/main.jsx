3879548473497dc6cdac6cd937bc0485db7d2db1
zakerous <m.z.humayoon@gmail.com>
2025-07-31 22:37:39 +0300

    I cant focussss

--- redux/src/main.jsx
+++ redux/src/main.jsx
@@ -2,16 +2,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 import noteReducer, { createNote } from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
 
-const store = createStore(reducer);
 console.log("Initializing Store: ", store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
